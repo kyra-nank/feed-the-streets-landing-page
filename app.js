dd5c4ef36f89f7430cb11f8ed7d56aaeb5c6433f
Kyra Nankivell <kyranankivell@Kyras-MacBook-Pro.local>
2021-10-18 01:31:58 -0400

    Finish rought front-end

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static('public')); // allows server to find static files
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(3000, function() {
   console.log('Server running on port 3000.')
