72c9dab9303526c7f7f145ef3d0a773e1b34348b
Kyra Nankivell <kyranankivell@Kyras-MacBook-Pro.local>
2021-10-18 14:10:35 -0400

    Add MailChimp, add success and failure pages, center content

--- app.js
+++ app.js
@@ -2,12 +2,66 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
 
 const app = express();
 app.use(express.static('public')); // allows server to find static files
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(req, res) {
+  const email = req.body.userEmail;
+
+  // need to match what MailChimp wants (this is JS)
+  const data = {
+    members: [{
+      email_address: email,
+      status: 'subscribed'
+    }]
+  }
+
+  const jsonData = JSON.stringify(data); // turn JS object into JSON
+
+  // from main MailChimp endpoint - find in API docs
+  url = 'https://us6.api.mailchimp.com/3.0/lists/b07638c29c'; // append 'lists' and list ID
+
+  const options = {
+    method: 'POST',
+    auth: 'kyra_nank:cacdf4ef74d8702aeded6185b2f4e0ae-us6' // any username, and my api key
+  }
+
+  // response will be from MailChimp
+  const request = https.request(url, options, function(response) {
+
+
+
+    response.on('data', function(data) {
+
+      if (response.statusCode === 200 && JSON.parse(data).errors[0] === undefined) {
+        res.sendFile(__dirname + '/success.html');
+      } else {
+        res.sendFile(__dirname + '/failure.html');
+      }
+      // console.log(JSON.parse(data).errors[0].error);
+      console.log(JSON.parse(data));
+    });
+  });
+
+  // pass JSON data to the MailChimp data
+  request.write(jsonData);
+  request.end();
+
+});
+
+// failure route for the Try Again button
+app.post('/failure', function(req, res) {
+  res.redirect('/');    // redirect back to root
 });
 
 app.listen(3000, function() {
