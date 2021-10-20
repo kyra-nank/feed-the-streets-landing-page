cdfa6fb853d9e7ba55feb73af68de0c17261549a
Kyra Nankivell <kyranankivell@Kyras-MacBook-Pro.local>
2021-10-20 01:15:47 -0400

    New MailChimp x Heroku setup

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const https = require('https');
+const mailchimp = require('@mailchimp/mailchimp_marketing');
 
 const app = express();
 app.use(express.static('public')); // allows server to find static files
@@ -11,52 +12,46 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+const listId = 'b07638c29c';
+
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: 'us6',
+});
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', function(req, res) {
-  const email = req.body.userEmail;
-
-  // need to match what MailChimp wants (this is JS)
-  const data = {
-    members: [{
-      email_address: email,
-      status: 'subscribed'
-    }]
+  console.log(req.body);
+  const emailAddress = req.body.emailAddress;
+  const subscribingUser = {
+    email: emailAddress,
   }
 
-  const jsonData = JSON.stringify(data); // turn JS object into JSON
-
-  // from main MailChimp endpoint - find in API docs
-  url = process.env.MAILCHIMP_URI
-  // url = 'https://us6.api.mailchimp.com/3.0/lists/' + process.env.MAILCHIMP_LIST_ID; // append 'lists' and list ID
-
-  const options = {
-    method: 'POST',
-    auth: 'kyra_nank:' + process.env.MAILCHIMP_API_KEY // any username, and my api key
-  }
-
-  // response will be from MailChimp
-  const request = https.request(url, options, function(response) {
-
-    response.on('data', function(data) {
-
-      if (response.statusCode === 200 && JSON.parse(data).errors[0] === undefined) {
-        res.sendFile(__dirname + '/success.html');
-      } else {
-        res.sendFile(__dirname + '/failure.html');
-      }
-      // console.log(JSON.parse(data).errors[0].error);
-      console.log(JSON.parse(data));
+async function run() {
+  try {
+    const response = await mailchimp.lists.addListMember(listId, {
+      email_address: subscribingUser.email,
+      status: 'subscribed',
+      // merge_fields would go here
     });
-  });
+    console.log(
+      `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+    );
+    res.sendFile(__dirname + '/success.html');
+  } catch (error) {
+    console.log(error);
+    res.sendFile(__dirname + '/failure.html');
+  }
+}
 
-  // pass JSON data to the MailChimp data
-  request.write(jsonData);
-  request.end();
+
 
 });
+
+run();
 
 // failure route for the Try Again button
 app.post('/failure', function(req, res) {
@@ -66,3 +61,51 @@ app.post('/failure', function(req, res) {
 app.listen(process.env.PORT || 3000, function() {
   console.log('Server running.')
 })
+
+
+
+
+
+// ORIGINAL - PASTE UNDER app.get(....... res.sendFile(....'/index.html'))
+// app.post('/', function(req, res) {
+//   const email = req.body.userEmail;
+//
+//   // need to match what MailChimp wants (this is JS)
+//   const data = {
+//     members: [{
+//       email_address: email,
+//       status: 'subscribed'
+//     }]
+//   }
+//
+//   const jsonData = JSON.stringify(data); // turn JS object into JSON
+//
+//   // from main MailChimp endpoint - find in API docs
+//   url = process.env.MAILCHIMP_URI
+//   // url = 'https://us6.api.mailchimp.com/3.0/lists/' + process.env.MAILCHIMP_LIST_ID; // append 'lists' and list ID
+//
+//   const options = {
+//     method: 'POST',
+//     auth: 'kyra_nank:' + process.env.MAILCHIMP_API_KEY // any username, and my api key
+//   }
+//
+//   // response will be from MailChimp
+//   const request = https.request(url, options, function(response) {
+//
+//     response.on('data', function(data) {
+//
+//       if (response.statusCode === 200 && JSON.parse(data).errors[0] === undefined) {
+//         res.sendFile(__dirname + '/success.html');
+//       } else {
+//         res.sendFile(__dirname + '/failure.html');
+//       }
+//       // console.log(JSON.parse(data).errors[0].error);
+//       console.log(JSON.parse(data));
+//     });
+//   });
+//
+//   // pass JSON data to the MailChimp data
+//   request.write(jsonData);
+//   request.end();
+//
+// });
